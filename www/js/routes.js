0029e842a297ea4102815b9bb193e448a5f96f45
RafaelLimaReis <rafaellimareis01@gmail.com>
2017-02-26 14:54:52 -0300

    routes.js - criação de arquivo principal de rotas

--- www/js/routes.js
+++ www/js/routes.js
@@ -3,7 +3,12 @@ angular.module('routes',[]).config(function($stateProvider, $urlRouterProvider, 
   $stateProvider
     .state('login', {
       url: '/login',
-      templateUrl: 'templates/login.html'
-    });
+      templateUrl: 'templates/login.html',
+      controller: 'userController'
+    })
+    .state('home',{
+      url:'/home',
+      templateUrl: 'templates/home.html'
+    })
     $urlRouterProvider.otherwise('/login');
-  });
+  })
