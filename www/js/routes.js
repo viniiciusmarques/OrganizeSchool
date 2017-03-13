10a9187cecdf17406d918959a78ee0abe216e3d4
RafaelLimaReis <rafaellimareis01@gmail.com>
2017-03-13 19:44:48 -0300

    routes.js - Novas rotas

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,14 +1,45 @@
 angular.module('starter')
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
+
+    .state('menu',{
+      url: '/menu',
+      templateUrl: 'templates/menu.html',
+      abstract: true
+    })
+
     .state('login', {
       url: '/login',
       templateUrl: 'templates/login.html',
       controller: 'userController'
     })
-    .state('home',{
+
+    .state('menu.home',{
       url:'/home',
-      templateUrl: 'templates/home.html'
+      views :{
+        'menuConteudo' : {
+          templateUrl: 'templates/home.html'
+        }
+      }
+    })
+
+    .state('menu.disciplinas',{
+      url:'/disciplinas',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/disciplinas.html',
+          controller: 'disciplinasController'
+        }
+      }
+    })
+
+    .state('menu.cadDisciplina',{
+      url:'/cadDisciplina',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/cadDisciplina.html'
+        }
+      }
     })
     $urlRouterProvider.otherwise('/login');
   })
